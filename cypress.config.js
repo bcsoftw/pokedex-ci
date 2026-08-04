319bee216d7ee24c2c94ca803ee2d99531b61ab8
Brayan Colina <bcolina88@gmail.com>
2026-08-04 13:01:55 -0400

    Change in timeout test e2e

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = {
   e2e: {
     baseUrl: 'http://localhost:5000',
     supportFile: false,
-    pageLoadTimeout: 60000,
+    pageLoadTimeout: 300000,
     // Aquí puedes definir tu baseUrl, carpetas o eventos si los necesitas
     setupNodeEvents(on, config) {
       return config
