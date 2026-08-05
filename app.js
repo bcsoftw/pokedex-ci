cdfe8ccbde2d51252bf5b616612b80dd7e22e14c
Brayan Colina <bcolina88@gmail.com>
2026-08-05 13:33:52 -0400

    Fix Deploy workflow

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// Ruta de health check simple
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
+
 app.listen(PORT, () => {
   process.stdout.write(`server started on port ${PORT}\n`)
 })
